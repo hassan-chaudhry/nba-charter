15d1e98cc97cf1d671b3f6f7344d5f60603e7a7f
hassan-chaudhry <hc2843@nyu.edu>
2025-04-10 00:37:05 -0400

    testing firebase config for deployment

--- api/firebase.config.js
+++ api/firebase.config.js
@@ -2,16 +2,22 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, "base64").toString(
-      "utf8"
-    )
-  );
+  try {
+    const serviceAccount = JSON.parse(
+      Buffer.from(
+        process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
+        "base64"
+      ).toString("utf8")
+    );
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://nba-shot-chart-default-rtdb.firebaseio.com",
-  });
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://nba-shot-chart-default-rtdb.firebaseio.com",
+    });
+  } catch (error) {
+    console.error("Firebase config error:", error);
+    throw new Error("Firebase Admin initialization failed.");
+  }
 }
 const db = admin.database();
 
